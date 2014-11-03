302a8ea72bd844af8bfba7d39cff0d974c740325
jraynal <jraynal@enseirb-matmeca.fr>
2014-11-03 17:26:46 +0100

    Brain up

---
+++ src/Nemesis/Brain/Chicken.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nemesis.Brain
+{
+    abstract class Chicken
+    { 
+        private struct OtherChicken {
+            public Chicken chick;
+            public int trust;
+        };
+
+        private List<OtherChicken> friends;
+        private List<OtherChicken> foes;
+
+        private int fear;
+
+        public Chicken(){}
+
+        private int randVal(int N) {
+            Random rg = new System.Random();
+            return rg.Next(N);
+        }
+
+        public void discoverArea(List<Chicken> mates) {
+            foreach (Chicken chick in mates ) {
+                int trust=randVal(100);
+                OtherChicken ochick=new OtherChicken();
+                ochick.chick = chick;
+                ochick.trust = trust;
+                if (trust > 50)
+                    friends.Add(ochick);
+                else
+                    foes.Add(ochick);
+            }
+        }
+
+        public void discuss();
+        public void vote();
+        public void decide();
+    }
+}
